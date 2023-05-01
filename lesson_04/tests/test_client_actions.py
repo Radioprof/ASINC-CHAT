7013afb072ac1f2d58f72c48089f2a49140833e0
Sergey <so.bunin@gmail.com>
2023-05-01 18:11:36 +0300

    Домашнее задание №4

--- lesson_04/tests/test_client_actions.py
+++ lesson_04/tests/test_client_actions.py
@@ -3,6 +3,7 @@ import sys
 import unittest
 import time
 sys.path.append(os.path.join(os.getcwd(), '..'))
+
 from tools.client_actions import auth, presence
 
 
@@ -13,29 +14,40 @@ class TestAnswerMessage(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_right_message(self):
+    def test_right_auth(self):
+        data = {
+                "action": "authenticate",
+                "time": time.time(),
+                "user": {
+                    "account_name": 'admin',
+                    "password": 'admin'
+                    }
+                }
+        self.assertEqual(auth('admin', 'admin'), data)
+
+    def test_right_presence(self):
+        data = {
+            "action": "presence",
+            "time": time.time(),
+            "type": "status",
+            "user": {
+                "account_name": 'guest',
+                "status": 'online'
+                }
+            }
+        self.assertEqual(presence('guest', 'online'), data)
+
+    def test_wrong_auth(self):
+        data = {
+                "action": "authenticate",
+                "time": time.time(),
+                "user": {
+                    "account_name": 'admin',
+                    "password": 'admin'
+                    }
+                }
+        self.assertEqual(auth('guest', '1234'), data)
 
 
-# def auth(name, password):
-#     data = {
-#         "action": "authenticate",
-#         "time": time.time(),
-#         "user": {
-#             "account_name": name,
-#             "password": password
-#             }
-#         }
-#     return data
-#
-#
-# def presence(name, status_mes=None):
-#     data = {
-#         "action": "presence",
-#         "time": time.time(),
-#         "type": "status",
-#         "user": {
-#             "account_name": name,
-#             "status": status_mes
-#             }
-#         }
-#     return data
+if __name__ == '__main__':
+    unittest.main()
